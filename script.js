e2541e43b74d3782dc9f9197b5474bb92b8f7262
이주석 <jooseok.lee@chicpap.com>
2024-12-11 15:07:35 +0900

    FIX: 네이버 로그인 세팅

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var params = new Proxy(new URLSearchParams(window.location.search), {
-  get: (searchParams, prop) => searchParams.get(prop),
-});
-var clientId = params.clientId;
-var callbackUrl = decodeURIComponent(params.callbackUrl);
-const naverLogin = new naver.LoginWithNaverId({
-  clientId: clientId,
-  callbackUrl: callbackUrl,
-  isPopup: false,
-  loginButton: { color: "green", type: 3, height: "0" },
-});
-naverLogin.init();
-document.getElementsByTagName("a")[0].click();
+// URL 파라미터 추출
+const params = new URLSearchParams(window.location.search);
+const clientId = params.get("clientId");
+const callbackUrl = decodeURIComponent(params.get("callbackUrl"));
+const returnedState = params.get("state");
+const storedState = localStorage.getItem("naver_state");
+if (returnedState !== storedState) {
+  console.error("Invalid state detected");
+  alert("State mismatch detected. Please try again.");
+} else {
+  const naverLogin = new naver.LoginWithNaverId({
+    clientId: clientId,
+    callbackUrl: callbackUrl,
+    isPopup: false,
+    loginButton: { color: "green", type: 3, height: "0" },
+  });
+  naverLogin.init();
+  document.getElementsByTagName("a")[0].click();
+}
